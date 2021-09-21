f3433dea9d082a4ecb602fe1f1a37dc06e33593f
v-haijianxia@creditease.cn <v-haijianxia@creditease.cn>
2021-09-21 11:53:56 +0800

    Initial commit

---
+++ src/main/java/com/xhj/txt/evilj/EvilJApplication.java
@@ -0,0 +1,12 @@
+package com.xhj.txt.evilj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvilJApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(EvilJApplication.class, args);
+  }
+}
---
+++ src/test/java/com/xhj/txt/evilj/EvilJApplicationTests.java
@@ -0,0 +1,11 @@
+package com.xhj.txt.evilj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvilJApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
